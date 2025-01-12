1a756d400c2de9bbcd9658ee6c70aa0d8fc59850
Ann <andra.ahmad.ft22@mail.umy.ac.id>
2025-01-13 02:04:15 +0700

    mendaftarkan fungsi pengelola halaman ke MainActivity.kt

--- app/src/main/java/com/example/pampertemuan14/MainActivity.kt
+++ app/src/main/java/com/example/pampertemuan14/MainActivity.kt
@@ -1,9 +1,12 @@
 package com.example.pampertemuan14
 
+import android.annotation.SuppressLint
+import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.annotation.RequiresExtension
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
@@ -11,19 +14,21 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.pampertemuan14.ui.navigation.PengelolaHalaman
 import com.example.pampertemuan14.ui.theme.PAMPertemuan14Theme
 
 class MainActivity : ComponentActivity() {
+    @RequiresExtension(extension = Build.VERSION_CODES.S, version = 7)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             PAMPertemuan14Theme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    PengelolaHalaman(
+                        modifier = Modifier.padding(innerPadding),
+
+                        )
                 }
             }
         }
