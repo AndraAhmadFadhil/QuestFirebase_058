24e130878bd9d91de54a932a6c09342a18e4cd2b
Ann <andra.ahmad.ft22@mail.umy.ac.id>
2025-01-13 00:30:35 +0700

    menambahkan class InsertViewModel untuk mengelola state dan logika form insert

--- app/src/main/java/com/example/pampertemuan14/ui/viewmodel/InsertViewModel.kt
+++ app/src/main/java/com/example/pampertemuan14/ui/viewmodel/InsertViewModel.kt
@@ -9,6 +9,65 @@ import com.example.pampertemuan14.model.Mahasiswa
 import com.example.pampertemuan14.repository.MahasiswaRepository
 import kotlinx.coroutines.launch
 
+class InsertViewModel(
+    private val mhs: MahasiswaRepository
+) : ViewModel() {
+
+    var uiEvent: InsertUiState by mutableStateOf(InsertUiState())
+        private set
+
+    var uiState: FormState by mutableStateOf(FormState.Idle)
+        private set
+
+    // Memperbarui state berdasarkan input pengguna
+    fun updateState(mahasiswaEvent: MahasiswaEvent) {
+        uiEvent = uiEvent.copy(
+            insertUiEvent = mahasiswaEvent,
+        )
+    }
+
+    // Validasi data input pengguna
+    fun validateFields(): Boolean {
+        val event = uiEvent.insertUiEvent
+        val errorState = FormErrorState(
+            nim = if (event.nim.isNotEmpty()) null else "NIM tidak boleh kosong",
+            nama = if (event.nama.isNotEmpty()) null else "Nama tidak boleh kosong",
+            jenis_kelamin  = if (event.jenis_kelamin.isNotEmpty()) null else "Jenis Kelamin tidak boleh kosong",
+            alamat = if (event.alamat.isNotEmpty()) null else "Alamat tidak boleh kosong",
+            kelas = if (event.kelas.isNotEmpty()) null else "Kelas tidak boleh kosong",
+            angkatan = if (event.angkatan.isNotEmpty()) null else "Angkatan tidak boleh kosong"
+
+        )
+
+        uiEvent = uiEvent.copy(isEntryValid = errorState)
+        return errorState.isValid()
+    }
+    fun insertMhs(){
+        if (validateFields()){
+            viewModelScope.launch {
+                uiState = FormState.Loading
+                try {
+                    mhs.insertMahasiswa(uiEvent.insertUiEvent.toMhsModel())
+                    uiState = FormState.Success("Data berhasil disimpan")
+                } catch (e: Exception){
+                    uiState = FormState.Error("Data gagal disimpan")
+                }
+            }
+        } else {
+            uiState = FormState.Error("Data tidak valid")
+        }
+    }
+    fun resetForm(){
+        uiEvent = InsertUiState()
+        uiState = FormState.Idle
+    }
+
+    fun resetSnacBarMessage(){
+        uiState = FormState.Idle
+    }
+
+}
+
 sealed class FormState{
     object Idle : FormState()
     object Loading : FormState()
