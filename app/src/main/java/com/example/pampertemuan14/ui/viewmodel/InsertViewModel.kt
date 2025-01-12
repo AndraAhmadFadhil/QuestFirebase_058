0c2bcc20315da244e73ea9d389f8a02809108f04
Ann <andra.ahmad.ft22@mail.umy.ac.id>
2025-01-13 00:01:01 +0700

    menambahkan data class mahasiswa event untuk representasi data mahasiswa

---
+++ app/src/main/java/com/example/pampertemuan14/ui/viewmodel/InsertViewModel.kt
@@ -0,0 +1,21 @@
+package com.example.pampertemuan14.ui.viewmodel
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.pampertemuan14.model.Mahasiswa
+import com.example.pampertemuan14.repository.MahasiswaRepository
+import kotlinx.coroutines.launch
+
+
+
+data class MahasiswaEvent(
+    val nim: String = "",
+    val nama: String = "",
+    val jenis_kelamin: String = "",
+    val alamat: String = "",
+    val kelas: String = "",
+    val angkatan: String = "",
+)
